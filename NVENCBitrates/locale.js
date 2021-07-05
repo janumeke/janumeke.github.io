001b75f229bd6f23bedb15991cc5840b9ef4eaac
Ryan <janumeke@gmail.com>
2021-07-05 16:09:05 +0800

    Adjust layout and formulae.

---
+++ NVENCBitrates/locale.js
@@ -0,0 +1,23 @@
+const LOCALIZATION = {
+    "TITLE": "位元速率計算器",
+    "LABEL": {
+        "RESOLUTION": "解析度",
+        "FRAMERATE": "FPS",
+        "GAMETYPE": {
+            0: "遊戲類型",
+            "2D": "2D",
+            "TOPDOWN": "俯視視角",
+            "THIRD": "第三人稱視角",
+            "FIRST": "第一人稱視角",
+        },
+        "PREFERENCE": {
+            0: "偏好",
+            "STABLE": "觀看穩定",
+            "LOWER": "更低位元速率",
+        },
+        "RATECONTROL": "位元率控制",
+        "BITRATE": "位元速率",
+        "MAXBITRATE": "最大位元速率",
+        "CALCULATE": "計算",
+    },
+};
