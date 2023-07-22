82cc0639943d67e9764c7ff1d62fba065f6a117e
Jao Yun-Jui <janumeke@gmail.com>
2023-07-22 11:42:12 +0800

    Updated ODTConfigurator

--- ODTConfigurator/click.js
+++ ODTConfigurator/click.js
@@ -4,8 +4,8 @@ function CapitalizeOnlyFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
+const INDENT = '    ';
 function PrettifyXML(element){
-    const indent = '    ';
     let xml = `<${element.tagName}`;
     const attributes = Array.from(element.attributes);
     attributes.forEach((attr) => xml += ` ${attr.name}="${attr.value}"`);
@@ -16,7 +16,7 @@ function PrettifyXML(element){
         xml += '>\n';
         children.forEach((child) => {
             let lines = PrettifyXML(child).split('\n');
-            lines = lines.map(line => indent + line);
+            lines = lines.map(line => INDENT + line);
             xml += `${lines.join('\n')}\n`;
         });
         xml += `</${element.tagName}>`;
@@ -25,6 +25,21 @@ function PrettifyXML(element){
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+    const source = document.querySelector('#source');
+    const version = document.querySelector('#version');
+    const edition = document.querySelector('#edition');
+    const channel = document.querySelector('#channel');
+    const download = document.querySelector('#download');
+    const cdnFallback = document.querySelector('#cdnFallback');
+    const productID = document.querySelector('#productID');
+    const languageID = document.querySelector('#languageID');
+    const display = document.querySelector('#display');
+    const eula = document.querySelector('#eula');
+    const pinTaskbar = document.querySelector('#pinTaskbar');
+    const update = document.querySelector('#update');
+    const updatePath = document.querySelector('#updatePath');
+    const updateChannel = document.querySelector('#updateChannel');
+    const output = document.querySelector('#output');
     let downloadUrl;
 
     document.querySelector('#generateBtn').addEventListener('click', () => {
@@ -32,28 +47,18 @@ document.addEventListener('DOMContentLoaded', () => {
         const configurationElement = xmlDoc.getElementsByTagName('Configuration')[0];
         
         const addElement = xmlDoc.createElement('Add');
-
-        const source = document.querySelector('#source');
         if(source.value)
             addElement.setAttribute(source.name, source.value);
-        const version = document.querySelector('#version');
         if(version.value)
             addElement.setAttribute(version.name, version.value);
-        const edition = document.querySelector('#edition');
         addElement.setAttribute(edition.name, edition.value);
-        const channel = document.querySelector('#channel');
         addElement.setAttribute(channel.name, channel.value);
-        const downloadPath = document.querySelector('#download');
-        if(downloadPath.value)
-            addElement.setAttribute(downloadPath.name, downloadPath.value);
-        const cdnFallback = document.querySelector('#cdnFallback');
+        if(download.value)
+            addElement.setAttribute(download.name, download.value);
         addElement.setAttribute(cdnFallback.name, CapitalizeOnlyFirstLetter(cdnFallback.checked));
-
         configurationElement.append(addElement);
 
         const productElement = xmlDoc.createElement('Product');
-
-        const productID = document.querySelector('#productID');
         let productID_value = '';
         for(let select = productID, options = PRODUCT_ID; select && options; options = options[select.value].options, select = select.nextElementSibling){
             if(typeof options[select.value] == 'string')
@@ -62,23 +67,15 @@ document.addEventListener('DOMContentLoaded', () => {
                 productID_value += options[select.value].value;
         }
         productElement.setAttribute(productID.name, productID_value);
-
         addElement.append(productElement);
 
         const languageElement = xmlDoc.createElement('Language');
-
-        const languageID = document.querySelector('#languageID');
         languageElement.setAttribute(languageID.name, LANGUAGE_ID[languageID.value]);
-
         productElement.append(languageElement);
 
         const displayElement = xmlDoc.createElement('Display');
-
-        const display = document.querySelector('#display');
         displayElement.setAttribute(display.name, display.value);
-        const eula = document.querySelector('#eula');
         displayElement.setAttribute(eula.name, String(eula.checked).toUpperCase());
-
         configurationElement.append(displayElement);
 
         const excludes = Array.from(document.querySelectorAll('#exclude input'));
@@ -91,27 +88,19 @@ document.addEventListener('DOMContentLoaded', () => {
         });
 
         const propertyElement = xmlDoc.createElement('Property');
-
-        const pinTaskbar = document.querySelector('#pinTaskbar');
         propertyElement.setAttribute('Name', pinTaskbar.name);
         propertyElement.setAttribute('Value', String(pinTaskbar.checked).toUpperCase());
-
         configurationElement.append(propertyElement);
 
         const updatesElement = xmlDoc.createElement('Updates');
-
-        const update = document.querySelector('#update');
         updatesElement.setAttribute(update.name, String(update.checked).toUpperCase());
-        const updatePath = document.querySelector('#updatePath');
-        if(updatePath.value)
-            updatesElement.setAttribute(updatePath.name, updatePath.value);
-        const updateChannel = document.querySelector('#updateChannel');
-        updatesElement.setAttribute(updateChannel.name, updateChannel.value);
-
+        if(update.checked){
+            if(updatePath.value)
+                updatesElement.setAttribute(updatePath.name, updatePath.value);
+            updatesElement.setAttribute(updateChannel.name, updateChannel.value);
+        }
         configurationElement.append(updatesElement);
 
-        let xml = new XMLSerializer().serializeToString(xmlDoc);
-        const output = document.querySelector('#output');
         output.value = PrettifyXML(configurationElement);
         if(downloadUrl)
             URL.revokeObjectURL(downloadUrl);
@@ -125,10 +114,10 @@ document.addEventListener('DOMContentLoaded', () => {
         navigator.clipboard.writeText(document.querySelector("#output").value);
     });
 
+    const downloadLink = document.createElement("a");
     document.querySelector("#downloadBtn").addEventListener("click", () => {
-        const anchor = document.createElement("a");
-        anchor.href = downloadUrl;
-        anchor.download = "configuration.xml";
-        anchor.click();
+        downloadLink.href = downloadUrl;
+        downloadLink.download = "configuration.xml";
+        downloadLink.click();
     });
 });
